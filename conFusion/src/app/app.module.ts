66e22577758569626b58b74358bc76834cdf50b3
MuhammadZain795 <bsef17m030@pucit.edu.pk>
2020-06-10 10:55:46 +0500

    angular components part-1

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -2,19 +2,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
 import { FlexLayoutModule } from '@angular/flex-layout';
 
 
 import { AppComponent } from './app.component';
 import 'hammerjs';
+import { MenuComponent } from './menu/menu.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
     BrowserAnimationsModule,
     MatToolbarModule,
+    MatListModule,
     FlexLayoutModule
   ],
   providers: [],
