bf366d5f6fd58e0f80b477524b949ff169bd786a
MuhammadZain795 <bsef17m030@pucit.edu.pk>
2020-06-11 10:12:59 +0500

    single page application part 1

--- conFusion/src/app/app-routing/routes.ts
+++ conFusion/src/app/app-routing/routes.ts
@@ -8,6 +8,7 @@ import { ContactComponent } from '../contact/contact.component';
 
 export const routes: Routes = [
   { path: 'home',  component: HomeComponent },
+  { path: 'contactus',     component: ContactComponent },
   { path: 'menu',     component: MenuComponent },
   { path: '', redirectTo: '/home', pathMatch: 'full' }
 ];
