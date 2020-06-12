540116ba7f8d8a2209b2163680c51df664c1ec0d
MuhammadZain795 <bsef17m030@pucit.edu.pk>
2020-06-12 09:35:48 +0500

    reactive form validation

--- conFusion/src/app/app.module.ts
+++ conFusion/src/app/app.module.ts
@@ -25,6 +25,9 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatInputModule } from '@angular/material/input';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { FormsModule } from '@angular/forms';
+import { MatSelectModule } from '@angular/material/select';
+import { MatSlideToggleModule } from '@angular/material/slide-toggle';
+import { ReactiveFormsModule } from '@angular/forms';
 @NgModule({
   declarations: [
     AppComponent,
@@ -45,13 +48,16 @@ import { FormsModule } from '@angular/forms';
     FlexLayoutModule,
     MatGridListModule,
     MatFormFieldModule,
+    MatSelectModule,
+    MatSlideToggleModule,
     MatInputModule,
     MatCheckboxModule,
     AppRoutingModule,
     MatDialogModule,
     MatCardModule,
     MatButtonModule,
-    FormsModule
+    FormsModule,
+    ReactiveFormsModule
   ],
   entryComponents: [
     LoginComponent
